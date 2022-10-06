c1fdf434c6061a96663fbbd9d0566e458189edc8
cline99 <54473001+cline99@users.noreply.github.com>
2022-10-06 23:26:59 +0530

    Update Constants.java

--- DefenseDroid Android/app/src/main/java/com/cms/defensedroid/util/Constants.java
+++ DefenseDroid Android/app/src/main/java/com/cms/defensedroid/util/Constants.java
@@ -2,9 +2,9 @@ package com.cms.defensedroid.util;
 
 public class Constants {
 
-    public static final String SERVER_IP = "127.0.0.1"; //Add your Cloud Machine IP Address Here
-    public static final String SERVER_PORT = "80"; //Add your Cloud Machine Port Here
+    public static final String SERVER_IP = "127.0.0.1";
+    public static final String SERVER_PORT = "80";
 
-    public static final String FIREBASE_STORAGE_ID = "gs://your-bucket-name-here"; //Add your Firebase Cloud Storage ID Here
+    public static final String FIREBASE_STORAGE_ID = "gs://your-bucket-name-here";
 
 }
